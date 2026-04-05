4af5435b696ee37933da24b80ba1c2a66b17d920
Console <aabhishek001122@gmail.com>
2026-04-05 15:47:54 +0530

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-PORT = int(os.environ.get("PORT", 8000))
-HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler).serve_forever()
